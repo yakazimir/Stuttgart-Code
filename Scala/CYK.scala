91dc985c780e48c4f1b1d3a411e3e096ea08c5c0
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-01-05 22:17:54 +0100

    gives out mostly correct output without Table -- one problem remaining is repeating words, e.g. she loves pizza WITH anchovies on break WITH meat (this was the same problem in the python version)

--- Scala/CYK.scala
+++ Scala/CYK.scala
@@ -13,22 +13,18 @@ object CYK {
   gram.init
   
   class Parser (gram:Grammar,in:Input, table:Chart){
+    
     val input = in.pos 
-
+    
     private def allSpan(k : Int, z : Int, j : Int){
       try {
 	var x = table.x((k,z)); var y = table.x((z,j)) 
-
 	def prod(pair : Set[Product]) = { 
 	  var ter : Set[Any] = Set() 
 	  for (i <- pair) ter += i.productElement(0);ter
 	}
-
-	println(prod(x));println(prod(y))
-	println(makeP(prod(x),prod(y)))
-       
+	table.pAdd((k,j),makeP(prod(x),prod(y)))
       }
-      
       catch { 
 	case eof: java.util.NoSuchElementException => 
 	  None
@@ -38,11 +34,9 @@ object CYK {
     private def inParse(i:Int,j:Int) { 
       var k : Int  = j - 2 
       while (k > - 1){ 
-	var z : Int = k+1; println(k) 
+	var z : Int = k+1
 	while (z < j) {
-	  allSpan(k,z,j)
-	  println("\t"+z)
-	  z += 1
+	  allSpan(k,z,j);z += 1
 	}
 	k -= 1
       }
@@ -52,42 +46,50 @@ object CYK {
       for (word <- input) {
 	if ((gram.tList).contains(word)){ 
 	  var i = input.indexOf(word); var j=i+1
-	  table.Add((i,j),word)
-	  inParse(i,j)
+	  table.lAdd((i,j),word); inParse(i,j)
 	}     
 	else {println("not in grammar")} 
       }     
     }
-  
   }
 
   class Input(x:String) { 
 
     val pos : List[String] = (x.split(" ")).toList
     val original = "input: "+ x
-
     def inWSpans {
       for (word <- pos) {
 	var f = pos.indexOf(word) 
 	print(f+" "+word+" ")
       }
       print(pos.length+"\n")
-    }
+    } 
     inWSpans
 
   }
 
   class Chart { 
 
-    val x:Map[Product,Set[Product]] = Map()
+    val x:Map[Product,Set[Product]] = Map()   
     
-    def Add(pos: Product, in : String) {
+    //section function should be merged with first
+
+    def lAdd(pos: Product, in : String) {
       (gram.revMap).get(in) match { 
 	case Some(entry) => x += (pos -> entry)
 	case None => println("an error occurred");break
       }
-
     }
+
+    def pAdd(pos: Product, in : Set[(Any,Any)]) { 
+      for (item <- in) { 
+	(gram.revMap).get(item) match { 
+	  case Some(entry) => x += (pos -> entry)
+	  case None => None
+	}
+      }      
+    }
+
   } 
   
   def main(args: Array[String]){ 
@@ -98,7 +100,7 @@ object CYK {
       var chart = new Chart
       var input = new Input(x.toLowerCase)
       var parser = new Parser(gram, input, chart)
-      parser.Parse
+      parser.Parse; println(chart.x)
       
     }    
   }
