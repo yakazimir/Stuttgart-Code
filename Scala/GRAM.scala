d224e3f013f7c5947d0d463b31a1a5b58d385b8f
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-01-14 14:35:25 +0100

    totally revised version of GRAM, using polymorphism on add function

--- Scala/GRAM.scala
+++ Scala/GRAM.scala
@@ -1,149 +1,56 @@
-
-import scala.collection.mutable.Set
+import scala.collection.mutable.{Set,Map,HashSet}
 
 object GRAM { 
   
-  abstract class Entry 
-  case class NT(rew:Product,p:Double) extends Entry 
-  case class T(t1:String, p:Double) extends Entry
+  abstract class Entry
+  case class R(binary:Product,prob:Double) extends Entry 
+  case class L(lexical:String,prob:Double) extends Entry
   
   class Grammar {
-
-    val productions = Map(
-      "S" -> List((("N","V"),1.0)), 
-      "V" -> List((("V","N"),0.4), 
-		  (("V","OP"),0.1),
-		  (("eats"),0.2), 
-		  (("loves"),0.1), 
-		  (("hates"),0.2)),
-		  //(("pizza"),0.2)),
-      "N" -> List((("N","P"),0.2), 
-		  (("she"),0.1), 
-		  (("he"),0.1), 
-		  (("pizza"),0.1),
-		  (("anchovies"),0.2),
-		  (("meat"),0.1), 
-		  (("bread"), 0.1), 
-		  (("cheese"),0.1)),
-      "P" -> List((("PP","N"),1.0)),
-      "PP"-> List((("without"),0.4),
-		  (("with"),0.2), 
-		  (("on"),0.4)),
-      "OP"-> List((("N","P"),1.0)))
-
-
-    val Nterminals = productions.keys
-    var tList : Set[Any] = Set()
-    var revMap : Map[Any,Set[Product]] = Map()
     
+    val productions : Map[String,List[Entry]] = Map(
+      "S" -> List(R(("N","V"),1.0)), 
+      "V" -> List(R(("V","OP"),0.1), 
+		  R(("V","N"),0.4), 
+		   L("eats",0.2), 
+		   L("loves",0.1), 
+		   L("hates",0.2), 
+		   L("pizza",0.2)), 
+      "N" -> List(R(("N","P"),0.2), 
+		   L("she",0.1), 
+		   L("he", 0.1), 
+		   L("pizza",0.1), 
+		   L("anchovies",0.2), 
+		   L("meat", 0.2), 
+		   L("bread",0.1), 
+		   L("cheese",0.1)), 
+      "P" -> List(R(("PP","N"),1.0)), 
+      "PP"-> List( L("without",0.4), 
+		   L("with",0.2), 
+		   L("on", 0.4)), 
+      "OP"-> List(R(("N","P"),1.0)))
 
-    def init  = for ((x,y) <- productions) {
-      for (item <- y){
-	tList += item._1
-	if (!revMap.contains(item._1)){ 
-	  revMap += (item._1 -> Set((x,item._2)))
+
+    val nTerminals = productions.keys  
+    val terminals = new HashSet[String]
+    val subList = new HashSet[Product]
+    var tReverseMap : Map[String,Set[Product]] = Map() 
+    var rReverseMap : Map[Product, Set[Product]] = Map()  
+
+
+    def init(){ 
+      def add[T](z:String,x:T,pr:Double,y:Map[T,Set[Product]]):Map[T,Set[Product]]={ 
+	if (!y.contains(x)) {y += (x -> Set((z,pr)))}
+	else {y(x) += ((z,pr))}
+	y }
+      for(z <- nTerminals){
+	for (part <- productions(z)){ 
+	  part match { 
+	    case x : R => add[Product](z,x.binary,x.prob,rReverseMap); subList += x.binary
+	    case y : L => add[String](z, y.lexical,y.prob,tReverseMap); terminals += y.lexical
+	  }	  
 	}
-	else {revMap(item._1) += ((x,item._2))}
       }
     }
-    
-
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      //else revMap(item._1) += 
-	//revMap(""
-      //}
-    //}
-  //}
-
- 
-
-       
-      //else: 
-	  
-      //revMap += (item._1 -> List(x,item._2))
-       
-       
-
-       
-
-
-
-
-
-
-
-
-
-
-    //return A : Set[Any
-//print(A)
- 
-
-  
-
-      //println(item._1)
-      //for (sitem <- item.productIterator){ 
-	//println(sitem._0)
-      //}
-   //  productions.get(name) match {
-//       case Some(code) => 
-// 	name + " has " + code 
-//       case None => 
-// 	"Unknown code " 
-//     }
-
-//var termMap:Map[Any,Any] = Map()
-    //productions foreach( 
-    // (t2) => t2.map(t2._1 -> t2._2))
-
-//var termMap:Map[String,Int] = _
-    //for ((t1,t2) <- productions) termMap += Map(t1 -> 2)   
-    //productions.foreach(t1 => termMap += Map(t1._1 -> t1._2))
-
-
-//   val productions = Map(
-//       "S" -> (NT(("N","V"),1.0)), 
-//       "V" -> (NT(("V","N"),0.6), 
-// 	      NT(("V","PP"),0.4),
-// 	       T(("eats"),0.4), 
-// 	       T(("loves"),0.3), 
-// 	       T(("hates"),0.3)),
-//       "N" -> (NT(("N","P"),0.2), 
-// 	       T(("she"),0.1), 
-// 	       T(("he"),0.1), 
-// 	       T(("pizza"),0.1),
-// 	       T(("pizza"),0.1),
-// 	       T(("anchovies"),0.1),
-// 	       T(("meat"),0.1), 
-// 	       T(("bread"), 0.1), 
-// 	       T(("cheese"),0.1)),
-//       "P" -> (NT(("PP","N"),1.0)),
-//       "PP"->  (T(("without"),0.4),
-// 	       T(("with"),0.2), 
-// 	       T(("on"),0.4)),
-//       "OP"-> (NT(("N","P"),1.0)))
-
-    // def Q = {
-//       println(x)
-//       println(y(0))
-//     }
-//     Q
