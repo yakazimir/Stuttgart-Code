5cd4aa406ce5b5694747b4038ad916c745cd8bc8
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-01-16 16:36:44 +0100

    newer version of parser file, almost to the point of printing out the most probable trees

--- Scala/Parser.scala
+++ Scala/Parser.scala
@@ -3,81 +3,50 @@ import GRAM.Grammar
 import scala.io.Source
 import scala.collection.mutable.{Set,Map}
 import java.util.Formatter._
+import scala.math._
 
 object Parser { 
+  
+  def time(f: => Unit)= {
+    val s = System.currentTimeMillis; f
+    System.currentTimeMillis - s
+    }
 
   val gram = new Grammar; gram.init
   val file = Source.fromFile("sentences.txt").getLines.toList
 
-  def time(f: => Unit)= {
-	val s = System.currentTimeMillis; f
-	System.currentTimeMillis - s
-  }
+  abstract class max 
+  case class rlVal(b:(Int,String,Int)) extends max 
+  case class rsVal(b:String) extends max
+  case class riVal(s:((Int,String,Int),(Int,String,Int))) extends max 
 
   class KnuthDijkstra(x: Chart, i : Input) { 
    
-    var maxVal : Map[Any,Double] = Map()
-    //private var terminals = i.original.split(" ").toSet
-    //private var forest = x.forest.reverse
+    var maxVal : Map[(Int,String,Int),(max,Double)] = Map()
 
-    private def maxCheck (p : Any, pr: Any, x : Any, y : Any){
-      var prob : Double = pr.asInstanceOf[Double] * maxVal(x) * maxVal(y) 
-      
-      try {
-	if (!(maxVal.contains(p) && maxVal(p)<prob)){
-	  maxVal += (p -> prob)
-	}
-      }
-      catch { 
-	case eof: java.util.NoSuchElementException => 
-	  maxVal += (p -> prob)
-      }
-    }
-
-    def mostProbable() {
-       var terminals = i.original.split(" ").toSet
-       var forest = x.forest.reverse
-      
+    def mostProbable(){
+      var forest = x.forest.reverse
       for (part <- forest) { 
-	try { 
-	  if (terminals.contains(part.productElement(1).asInstanceOf[String])){
-	    maxVal += (part.productElement(0) ->
-		       part.productElement(2).asInstanceOf[Double])
-	  }
-	}     
-	catch { 
-	  case eof: java.lang.ClassCastException => 
-	    if (maxVal.contains(part.productElement(1))){
-	      var max : Double  = 
-		part.productElement(2).asInstanceOf[Double] * maxVal(part.productElement(1))
-	      maxVal += (part.productElement(0) -> max)
-	      
+	part match { 
+	  case x : lexical => 
+	    maxVal += (x.l._1 -> (rsVal(x.l._2),x.l._3))
+	  case y : singl => 
+	    maxVal += ((y.b._1) -> (rlVal(y.b._2),y.b._3))
+	  case z : binary => 
+	    var prob : Double = maxVal(z.bi._2._1)._2 * maxVal(z.bi._2._2)._2;
+	    try {
+	      if (!maxVal.contains(z.bi._1) && maxVal(z.bi._1)._2 > prob){
+		maxVal += (z.bi._1 -> (riVal((z.bi._2._1,z.bi._2._2)),prob * z.bi._3))
+	      }
+	    } 
+	    catch { 
+	      case eof: java.util.NoSuchElementException => 
+		maxVal += (z.bi._1 -> (riVal((z.bi._2._1,z.bi._2._2)),prob * z.bi._3))
 	    }
-	    else { 
-	      val (x,y) = part.productElement(1)
-	      maxCheck(part.productElement(0),part.productElement(2),x,y)      
-	    }
-	  
 	}
       }
     }
-    
-    def printMostProbable {
-      val string = "%-15s ==> %-20s";
-
-      for (entry <- x.forest){ 
-	//println(entry.productElement(0))
-	if (maxVal.contains(entry.productElement(0))) { 
-	  println("\t"+string.format(entry.productElement(0),maxVal(entry.productElement(0))))
-	}
-
-
-      }
-    }
-
-
   }
-
 
   def main(args: Array[String]){
     for (sentence <- file) { 
@@ -86,15 +55,17 @@ object Parser {
       var input = new Input(sentence.toLowerCase) 
       var parser = new Parser(gram, input, chart) 
       var knuth = new KnuthDijkstra(chart, input)
-      var timeFun : Double = time(parser.Parse)
-      var timeMostProb : Double = time(knuth.mostProbable())
+      
+      var timeFun : Double = time(parser.Parse())
+   
 
       print("INPUT: "); input.inWSpans
-      println("CPU Time: " + (timeFun+timeMostProb) / 1000.0) 
-      knuth.printMostProbable
+      println("CPU Time: " + (timeFun) / 1000.0) 
       
-      //var knuth = new KnuthDijkstra(chart, input) 
+      knuth.mostProbable()
       //chart.printForest
+      //println("==========================")
+      //knuth.printMostProbable
 
     }
   
