2198f4ab42d6f4688b49fc9ad95b24fd2c2fac17
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-02-06 11:51:04 +0100

    xml reader should work now

--- Scala/reader.scala
+++ Scala/reader.scala
@@ -4,80 +4,78 @@ import scala.xml.pull.{XMLEventReader,EvElemStart, EvElemEnd, EvText,XMLEvent}
 
 object reader {
   
-  abstract class Rule 
-  case class l(r:((Int,String,Int),String,Double)) extends Rule 
-  case class b(r:((Int,String,Int),((Int,String,Int),(Int,String,Int)),Double)) extends Rule
+  abstract class Rule { 
+    def head : Any = { }
+    def dep : Any = { }
+    def ruleType:Any = { } 
+  }
+  case class l(r:((Int,String,Int),String,Double)) extends Rule { 
+    override def head : (Int,String,Int) = {r._1}
+    override def dep : String = {r._2}
+    override def ruleType : (String,String) = {(r._1._2,r._2)}
+  }
+  case class b(r:((Int,String,Int),((Int,String,Int),(Int,String,Int)),Double)) extends Rule {
+    override def head : (Int,String,Int) = {r._1}
+    override def dep : ((Int,String,Int),(Int,String,Int)) = {r._2}
+    override def ruleType : (String,String,String) = {(r._1._2,r._2._1._2,r._2._2._2)}    
+  }
   case class prule(r:((Int,String,Int),Double)) extends Rule 
   case class lex(r:(Int,String,Int)) extends Rule 
-
 
   var readingText = false 
 
   val src = Source.fromFile("forest2.xml")
   val er = new XMLEventReader(src)
 
-
   private def getAttrVals(x : String, attributes:xml.MetaData) : Rule = { 
 
     var st = attributes.apply("start").toString.toInt
     var sp = attributes.apply("span").toString.toInt
-    var cat = attributes.apply("cat").toString
         
     x match { 
       case "Binary"|"Unary" => { 
+	var cat = attributes.apply("cat").toString 
 	var weight = attributes.apply("weight").toString.toDouble
 	return prule(((st,cat,sp),weight))
       }
-      case _ => 
+      case "C1"|"C2" => {
+	var cat = attributes.apply("cat").toString
+	return lex((st,cat,sp))
+      }
+      case "terminal" => {
 	var word = attributes.apply("word").toString
 	return lex((st,word,sp))
-    }
+      }
+    } 
   }
 
+
   private def sentenceEvent(ev : XMLEventReader) {
-    def toFormat(ty : String, sev : XMLEventReader, h : Rule) { 
+    def toFormat(des : String, sev : XMLEventReader): Rule =   { 
       var done2 = false
+      var result : Rule = lex((0,"f",0))
       while (sev.hasNext && !done2){
-	if (ty == "Binary"){ 
 	  sev.next match {
-	    case EvElemStart(_,"C1",atrs,_) => { 
-	      //getAttrVals("C1",atrs)
-	    }
-	    case EvElemStart(_,"C2",atrs,_) => {
-	      //println(getAttrVals("C2",atrs))
-	    }
-	    case EvElemEnd(_,"Binary") => {
-	      println("binary end")
+	    case EvElemStart(_,des,atrs,_) => {
+	      result = getAttrVals(des,atrs)
 	      done2 = true 
-	    } 
-	    case _ => 
-	  }
-	}
-	else {
-	  sev.next match {
-	    case EvElemStart(_,"terminal",atrs,_) => {
-	      //val first = getAttrVals("terminal",atrs)
-	    }
-	    case EvElemEnd(_,"Unary") => {
-	      done2 = true 
-	      println("unary end")
 	    }
 	    case _ => 
 	  }
-	
-	}
       }
+      return result 
     }
     var done = false 
     while (ev.hasNext && !done) {
       ev.next match {
 	case EvElemStart(_,"Binary",atr,_) => {
 	  val headRule = getAttrVals("Binary",atr)
-	  toFormat("Binary",ev, headRule)
+	  var r1 = toFormat("C1",ev)
+	  val r2 = toFormat("C2",ev)
 	}
 	case EvElemStart(_,"Unary",atr,_) => {
 	  val headRule = getAttrVals("Unary",atr)
-	  toFormat("Unary", ev, headRule)
+	  var terminal = toFormat("Unary", ev)
 	}
 	case EvElemEnd(_,"Rules") => {
 	    done = true 
@@ -103,8 +101,6 @@ object reader {
       }
     }
     
-
-
   }
 }
 
