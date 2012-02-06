fe8f9cc3c152247a67008986509a1061aa7963b9
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-02-07 00:01:55 +0100

    insideOut probabilities work, and look correct

--- Scala/reader.scala
+++ Scala/reader.scala
@@ -1,22 +1,29 @@
 import scala.xml._
 import scala.io.Source
 import scala.xml.pull.{XMLEventReader,EvElemStart, EvElemEnd, EvText,XMLEvent}
+import scala.collection.mutable.{Set,Map, HashSet}
 
 object reader {
   
   abstract class Rule { 
-    def head:(Int,String,Int)= (0," ",0);
-    def dep:Any={ };def ruleType:Any={ };def prob:Double=0.0   
+    def head:(Int,String,Int)= (0," ",0); def headLoc:(Int,Int)={(0,0)}
+    def prob:Double=0.0   
   }
   case class l(r:((Int,String,Int),(Int,String,Int),Double)) extends Rule { 
     override def head : (Int,String,Int) = {r._1}
-    override def dep : String = {r._2._2}
-    override def ruleType : (String,String) = {(r._1._2,r._2._2)}
+    override def headLoc:(Int,Int) = {(r._1._1,r._1._3)} 
+    override def prob : Double = {r._3}
+    def dep : (Int,String,Int) = {r._2}; 
+    def ruleType : (String,String) = {(r._1._2,r._2._2)}
   }
   case class b(r:((Int,String,Int),((Int,String,Int),(Int,String,Int)),Double)) extends Rule {
-    override def head : (Int,String,Int) = {r._1}
-    override def dep : ((Int,String,Int),(Int,String,Int)) = {r._2}
-    override def ruleType : (String,String,String) = {(r._1._2,r._2._1._2,r._2._2._2)}    
+    override def head : (Int,String,Int) = {r._1}; 
+    override def headLoc:(Int,Int) = {(r._1._1,r._1._3)} 
+    override def prob : Double = {r._3}
+    def ruleType : (String,String,String) = {(r._1._2,r._2._1._2,r._2._2._2)} 
+    def dep : ((Int,String,Int),(Int,String,Int)) = {r._2}
+    def dep1 : (Int,String,Int) = dep._1
+    def dep2 : (Int,String,Int) = dep._2 
   }
   case class prule(r:((Int,String,Int),Double)) extends Rule { 
     override def head : (Int,String,Int) = r._1
@@ -25,6 +32,10 @@ object reader {
   case class lex(r:(Int,String,Int)) extends Rule {
     override def head : (Int,String,Int) = {r}
   }
+  case class rule1(r:((String,String,String),Double)) extends Rule  
+  case class rule2(r:((String,String),Double)) extends Rule 
+  case class lo(r:(Int,Int)) extends Rule 
+  case class lo2(r:((Int,Int),(Int,Int),(Int,Int))) extends Rule 
 
   var readingText = false 
 
@@ -82,7 +93,7 @@ object reader {
 	  var terminal = toFormat("Unary", ev).head
 	  forest = l((headRule.head,terminal,headRule.prob))::forest
 	}
-	case EvElemEnd(_,"Rules") => {
+	case EvElemEnd(_,"sentence") => {
 	    done = true 
 	}
 	case _ =>       
@@ -90,18 +101,102 @@ object reader {
     }
     return forest 
   }
- 
+
+  class insideOutProb(x : List[Rule], size: Int) { 
+    
+    var inside : Map[(Int,Int),Map[String,Double]] = Map() 
+    var outside : Map[(Int,Int),Map[String,Double]] = Map() 
+    var rules : Map[Rule,List[Rule]] = Map() 
+
+    private def checkExist(x:(Int,Int),y:String,y2:Double,z:Map[(Int,Int),Map[String,Double]]){   
+      if (!z.contains(x)){
+	z += (x -> Map(y -> y2))
+      }
+    else {
+      if (z(x).contains(y)){
+	var upd : Double = z(x)(y) + y2
+	z(x) += (y -> upd)
+      } 
+      else {z(x) += (y -> y2)}
+    }
+   }
+    
+    private def insideProb():Map[(Int,Int),Map[String,Double]] = {   
+      for (item <- x){ 
+	item match { 
+	  case x : l => {
+	    checkExist(x.headLoc,x.head._2, x.prob,inside)
+	}
+	  case y : b => { 
+	    var f : Double = (y.prob * inside((y.dep1._1,y.dep1._3))(y.dep1._2)*
+			      inside((y.dep2._1,y.dep2._3))(y.dep2._2))
+	      checkExist(y.headLoc,y.head._2,f,inside)
+	  }
+	  case _ => 	
+	}
+      }
+      return inside 
+    }
+
+    private def outsideProb() = { 
+      def addRuleType(r:Rule,pos:Rule){
+	if (!rules.contains(r)){
+	  rules += (r -> List(pos))
+	}
+	else {rules(r) = pos::rules(r)}	
+      } 
+      def rec(fir:(Int,String,Int),sec:((Int,String,Int),(Int,String,Int)),prob:Double) {
+	checkExist(
+	  (sec._1._1,sec._1._3),sec._1._2,prob*(inside(sec._2._1,sec._2._3)(sec._2._2))*
+	  outside(fir._1,fir._3)(fir._2),outside) 	
+	checkExist(
+	  (sec._2._1,sec._2._3),sec._2._2,prob*(inside(sec._1._1,sec._1._3)(sec._1._2))*
+	  outside(fir._1,fir._3)(fir._2),outside)	
+      }
+      for (item <- x.reverse) { 
+	item match { 
+	  case x : l => 
+	    addRuleType(rule2((x.ruleType,x.prob)),lo(x.headLoc))
+	  case y : b => 
+	    addRuleType(rule1((y.ruleType, y.prob)),
+			      lo2((y.headLoc,(y.dep1._1,y.dep1._3),(y.dep2._1,y.dep2._3))))
+	    if (y.head == (0,"S",size)){
+	      checkExist(y.headLoc,y.head._2,1.0,outside)
+	      rec(y.head,y.dep,y.prob)
+	    }
+	    else { 
+	      rec(y.head,y.dep,y.prob)
+	    }
+	  case _ => 
+	}	
+      }
+    }
+
+    var insideVals = insideProb() 
+    var outsideVals = outsideProb()
+
+    //for (item<-insideVals)println(item)
+    println("========")
+    for (item<-outside)println(item)
+
+   
+  }
+
   def main(args: Array[String]) {
 
     val src = Source.fromFile("forest2.xml")
     val er = new XMLEventReader(src)
+    var inputSize : Int = 0
   
     while (er.hasNext){
       er.next match {
-	case EvElemStart(_,"sentence",attr,_) => 
-	  sentenceEvent(er)
-	case EvElemEnd(_,"sentence") =>
-	  println("sentence ended") 
+	case EvElemStart(_,"Length",attr,_) => 
+	  inputSize = attr.apply("lVal").toString.toInt
+	case EvElemStart(_,"Rules",_,_) => 
+	  var forest = sentenceEvent(er)
+	  var insideO = new insideOutProb(forest, inputSize)
+	case EvElemEnd(_,"trainingData") =>
+	  println("end training") 
 	case _ => 
       }
     }
