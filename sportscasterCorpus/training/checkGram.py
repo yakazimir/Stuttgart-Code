2452424d3f95eb3831fa84e9e771696afa9aede0
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-02-16 15:53:01 +0100

    minor changes

--- sportscasterCorpus/training/checkGram.py
+++ sportscasterCorpus/training/checkGram.py
@@ -10,13 +10,19 @@ sum = 0
 
 
  
+
 for line in grammar:
     leftRule = re.search(left,line)
     if leftRule.groups()[0] != currentR: 
         if sum < .99: 
-            print line
+            if sum != 0: 
+                print "!!!!!" 
+                break 
+            else: 
+                pass 
         elif sum > 1.00000000001: 
             print "!!!!!"
+            break 
         sum = 0 
         sum += float((leftRule.groups()[1].split(" "))[-1])
         currentR = leftRule.groups()[0]
