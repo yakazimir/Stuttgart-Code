dfc261f4567ebbd5113083f6b31413018621fea4
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-05-31 23:21:38 +0200

    changes to py script

--- latticeGrammar/getContexts.py
+++ latticeGrammar/getContexts.py
@@ -1,39 +1,57 @@
 import re 
 from sets import Set
 
-file = open('sentences.txt') 
-sentences = [i.split() for i in open('sentences.txt')] 
+file = open('sentencesSports.txt') 
+sentences = [i.split() for i in open('sentencesSports.txt')] 
 contexts = Set(); objects = {}
 
 #X = Set()
-
+to = 0
 for l in sentences:
 
+    print ("START",' '.join(l),"END")
 
     for i in range(len(l)): 
         
-        ##getting full word length 
-
-
-        if i-1 == -1: le = "\\b" ; m = l[i]
-        else: le = l[i-1]; m = l[i]
-
-        try: r = l[i+1] 
-        except IndexError: r = "\\e"  
-
-        c = (le,r); #print c,; print m
+        ### FOR PRODUCING 'SYNTACTIC CONCEPTS' 
+        ### IN SENSE OF CLARK
         
-        f = re.sub(', ',',',str(c))
 
-        if m in objects.keys(): 
-            objects[m].add(f) 
-        else: objects[m] = Set([f])
+        m = l[i]
+        if l[:i] == [] : le = "END"
+        else: le = ' '.join(l[:i]) 
+
+        if l[i+1:] == [] : r = "START" 
+        else: r = ' '.join(l[i+1:])
+
+        #print le,; print m,; print r
+
+        print (le,m,r)
 
 
-        #contexts.append(str(c))
-        contexts.add(f)
+        ###FOR PRODUCING WORD DISTRIBUTIONS 
 
-#p = 0
+        # ##getting full word length 
+        # if i-1 == -1: le = "\\b" ; m = l[i]
+        # else: le = l[i-1]; m = l[i]
+
+        # try: r = l[i+1] 
+        # except IndexError: r = "\\e"  
+
+        # c = (le,r); #print c,; print m
+        
+        # f = re.sub(', ',',',str(c))
+
+        # if m in objects.keys(): 
+        #     objects[m].add(f) 
+        # else: objects[m] = Set([f])
+
+
+        # #contexts.append(str(c))
+        # contexts.add(f)
+
+
+####PRINTING LIST FOR FC PROGRAM
 print "######FUTBOL CONTEXT FILE###"
 print "####ATTRIBUTES"
 print "attributes:",
@@ -49,18 +67,15 @@ print "################"
 for (i,j) in objects.items() : 
     print i+"--"+" ".join(j)
 
+
+######################
+###FOR TESTING ACCURACY
+######################
 #print contexts
 #print objects
-
 # for (key,value) in objects.items(): 
-    
 #     for item in value: 
-
 #         if not item in contexts: 
-
 #             print item
-
-
-
-print len(contexts)
+#print len(contexts)
 #print p
