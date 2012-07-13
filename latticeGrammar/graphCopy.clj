471b24fbaccf3ff1968f8bca32f5b4c1276ff28d
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-07-13 17:21:23 +0200

    made changes to graphyCop

--- latticeGrammar/graphCopy.clj
+++ latticeGrammar/graphCopy.clj
@@ -1,12 +1,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; FINDING NETWORK COMMUNITIES  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use '[clojure.java.io :only (reader)])
 (use '[clojure.string :only (split)])
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; READING INPUT FILE ;;
+;----------------------;
+
 (defn re-match? [re s] (not (nil? (re-find re s))))
 (defn attr? [s] (re-match? #"^attributes:" s)) 
 (defn object? [s] (re-match? #"^objects:" s))
 (defn objectV? [s] (re-match? #"^\w+\-\-" s))
+(defn in? [seq elem] (some #(= elem %) seq))
+
 (defn calcV[l]
   (defn extrE [p]
     (let [sl (split p #"--")
@@ -18,6 +26,7 @@
     (catch Exception e
       (prn "val format issue")
       (java.lang.System/exit 0))))
+
 (defn getAO [l pred]
   (try 
     (let [v (split (first (filter pred l))#":")
@@ -25,24 +34,35 @@
     (catch Exception e
       (prn "object/attr err")
       (java.lang.System/exit 0))))
+
+(defn breakU [tar lh]
+  
+  
+  (let [toM (map #(hash-map % '()) tar)]
+    (println toM)))
+       
 (defn compileGraph [atr objects objV]
   (println [atr objects objV])
   (let [v (map #(first (keys %)) objV)
         zw (map #(first (vals %)) objV)
-        zi (reduce concat zw) 
+        zi (reduce concat zw)       
         cor (and (= (set v) (set objects))
                  (= (set zi) (set atr)))]
     (case cor
       false (do (prn "match issue")
                 (java.lang.System/exit 0))
-      true (do (println v)))
-    (println zi)))
+      true ()) (breakU atr objV)))
+
 (defn readF [f]
   (def x (line-seq (reader f)))
   (let [atr (getAO x attr?)
         objects (getAO x object?)
         objV (calcV (filter objectV? x))]
-    (do
-      (compileGraph atr objects objV))))
+    (compileGraph atr objects objV)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; COMPUTING COMMUNITIES ;;
+;------------------------;;
 
 (time (readF "example.txt"))
