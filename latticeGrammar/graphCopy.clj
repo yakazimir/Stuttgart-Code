6a2743411359037c7542620fef0e45ec371fb5ed
Kazimir Richardson <kazimir.richardson@gmail.com>
2012-07-15 23:09:32 +0200

    now works properly

--- latticeGrammar/graphCopy.clj
+++ latticeGrammar/graphCopy.clj
@@ -19,7 +19,7 @@
   (defn extrE [p]
     (let [sl (split p #"--")
           keyi (first sl)
-          vali (split (first (rest sl)) #" ")
+          vali (split (first (rest sl)) #";")
           mapi (hash-map keyi vali)] mapi))
   (try 
     (let [remap (map #(extrE %) l)] remap)
@@ -30,19 +30,13 @@
 (defn getAO [l pred]
   (try 
     (let [v (split (first (filter pred l))#":")
-          g (split (nth v 1) #" ")] g)
+          g (split (nth v 1) #";")] g)
     (catch Exception e
       (prn "object/attr err")
       (java.lang.System/exit 0))))
 
-(defn breakU [tar lh]
-  
-  
-  (let [toM (map #(hash-map % '()) tar)]
-    (println toM)))
-       
-(defn compileGraph [atr objects objV]
-  (println [atr objects objV])
+;sanity check, probably not necessary
+(defn doubleCheck [atr objects objV]
   (let [v (map #(first (keys %)) objV)
         zw (map #(first (vals %)) objV)
         zi (reduce concat zw)       
@@ -50,19 +44,48 @@
                  (= (set zi) (set atr)))]
     (case cor
       false (do (prn "match issue")
+                (println [(set v) (set objects)])
+                (println [(set zi) (set atr)])
                 (java.lang.System/exit 0))
-      true ()) (breakU atr objV)))
+      true ())))
+
+(defn buildRev [l]
+  (defn kv [bag [k v]]
+    (update-in bag [k] conj v))
+  (defn mergeMatches [mList]
+    (reduce #(reduce kv %1 %2) {} mList))
+  (let [hh (map #(for [i (last %)]
+                   (hash-map i (first %))) l)
+        gh (mergeMatches (reduce concat hh))] gh))
 
 (defn readF [f]
   (def x (line-seq (reader f)))
   (let [atr (getAO x attr?)
         objects (getAO x object?)
-        objV (calcV (filter objectV? x))]
-    (compileGraph atr objects objV)))
-
+        objF (calcV (filter objectV? x))
+        objectM (into {} objF)
+        attributeM (buildRev objectM)]
+    (doubleCheck atr objects objF)
+    [objectM attributeM]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; COMPUTING COMMUNITIES ;;
 ;------------------------;;
 
+(flush)
+(println "reading input file")
 (time (readF "example.txt"))
+(time (readF "synConcepts.txt"))
+
+
+
+
+
+;(time (readF "synConcepts.txt"))
+
+;; (reduce concat (map #(for [i (last %)] (hash-map i (first %))) x))
+
+;; (defn kv [bag [k v]] 
+;;   (update-in bag [k] conj v))
+;; (defn mergeMatches [propertyMapList]
+;;   (reduce #(reduce kv %1 %2) {} propertyMapList))
